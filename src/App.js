b565bd0daa1fcffe14ac6dfeb9e75b44d4abfaa9
Samuel George <my@pers.on.al>
2023-09-27 09:29:57 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home.jsx';
 import Joke from './components/Joke';
-import First from './components/First.jsx'; 
-import Mine from './components/Mine.jsx';
 
 
 function App() {
@@ -12,10 +10,8 @@ function App() {
       <header className="App-header">
        <BrowserRouter>
           <Routes>
-            <Route path='home' element={<Home />} />
-            <Route path='/' element={<First />} />
+            <Route path='/' element={<Home />} />
             <Route path='joke' element={<Joke />} />
-            <Route path='emma' element={<Mine />} />
           </Routes>
        </BrowserRouter>
       </header>
